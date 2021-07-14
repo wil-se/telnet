495c5c4232938df6c825c10305104ee2a23665c5
William <sebastiani.1753672@studenti.uniroma1.it>
2021-07-14 11:10:48 +0200

    fix

--- telnet/telnet/settings.py
+++ telnet/telnet/settings.py
@@ -9,14 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from .remote import *
 from pathlib import Path
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +24,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SECRET_KEY = '49mfu#v90%s8$ek*=*+s^yw#tl!t4mp%%$#5u+0thkiiyu(+yy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '5.196.23.27']
 LOGIN_REDIRECT_URL = '/dashboard'
@@ -91,15 +89,6 @@ WSGI_APPLICATION = 'telnet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'telnet',
-        'USER': 'telnet',
-        'PASSWORD': 'telnet',
-        'HOST': '127.0.0.1',
-    }
-}
 
 
 # Password validation
